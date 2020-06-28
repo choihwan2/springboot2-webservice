410f366b704f11b751f42fdabf1f53e2dc5b17ed
Hwan <choihwan2@naver.com>
2020-06-28 18:46:41 +0900

    ADD: 3-5 code

---
+++ src/main/java/com/choihwan2/book/springboot2/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.choihwan2.book.springboot2.service.posts;
+
+import com.choihwan2.book.springboot2.domain.posts.PostsRepository;
+import com.choihwan2.book.springboot2.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
