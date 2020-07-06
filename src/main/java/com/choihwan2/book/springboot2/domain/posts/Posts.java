43b5efda0d961765190f063d55c793f50a09704c
Hwan <choihwan2@naver.com>
2020-07-06 19:13:56 +0900

    Add BaseTimeEntity

--- src/main/java/com/choihwan2/book/springboot2/domain/posts/Posts.java
+++ src/main/java/com/choihwan2/book/springboot2/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.choihwan2.book.springboot2.domain.posts;
 
+import com.choihwan2.book.springboot2.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
