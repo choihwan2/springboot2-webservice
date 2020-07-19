5c25182d1880f8e1f538096b32a73446eb50af74
Hwan <choihwan2@naver.com>
2020-07-20 00:35:19 +0900

    Finish Chapter4 code

--- src/main/java/com/choihwan2/book/springboot2/service/posts/PostsService.java
+++ src/main/java/com/choihwan2/book/springboot2/service/posts/PostsService.java
@@ -42,4 +42,10 @@ public class PostsService {
     public List<PostsListResponseDto> findAllDesc() {
         return postsRepository.findALlDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
+    }
 }
--- src/main/java/com/choihwan2/book/springboot2/web/indexController.java
+++ src/main/java/com/choihwan2/book/springboot2/web/indexController.java
@@ -1,10 +1,12 @@
 package com.choihwan2.book.springboot2.web;
 
 import com.choihwan2.book.springboot2.service.posts.PostsService;
+import com.choihwan2.book.springboot2.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +23,12 @@ public class indexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+        return "posts-update";
     }
 }
