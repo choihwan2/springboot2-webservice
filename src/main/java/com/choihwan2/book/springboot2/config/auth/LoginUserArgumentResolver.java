8ffd323b399c80672c4e4aff5af97d7d1829f65d
Hwan <choihwan2@naver.com>
2020-10-17 23:00:58 +0900

    Add LoginUser.java and Update LoginSession
    반복 호출이 될 수 있는 HttpSession 에서 로그인 정보를 받아오던 것을 어노테이션 기반으로 변경하여 어디서나 @LoginUser 호출로 세션에 들어있 로그인 정보를 받아올 수 있게 코드를 수정.

---
+++ src/main/java/com/choihwan2/book/springboot2/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,31 @@
+package com.choihwan2.book.springboot2.config.auth;
+
+import com.choihwan2.book.springboot2.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+    private final HttpSession httpSession;
+
+
+    @Override
+    public boolean supportsParameter(MethodParameter methodParameter) {
+        boolean isLoginUserAnnotation = methodParameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClass = SessionUser.class.equals(methodParameter.getParameterType());
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest nativeWebRequest, WebDataBinderFactory webDataBinderFactory) throws Exception {
+        return httpSession.getAttribute("user");
+    }
+}
--- src/main/java/com/choihwan2/book/springboot2/web/IndexController.java
+++ src/main/java/com/choihwan2/book/springboot2/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.choihwan2.book.springboot2.web;
 
+import com.choihwan2.book.springboot2.config.auth.LoginUser;
 import com.choihwan2.book.springboot2.config.auth.dto.SessionUser;
 import com.choihwan2.book.springboot2.service.posts.PostsService;
 import com.choihwan2.book.springboot2.web.dto.PostsResponseDto;
@@ -16,13 +17,11 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts",postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null){
             model.addAttribute("userName", user.getName());
         }
